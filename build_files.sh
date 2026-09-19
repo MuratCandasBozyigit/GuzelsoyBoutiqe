a118dd6e37b8f10c64c654983702f3189bbb9b27
muratcandaş <bozyigit31@gmail.com>
2026-09-19 23:45:39 +0300

    fix(vercel): simplify vercel.json build and add --break-system-packages

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 -m pip install --break-system-packages -r requirements.txt || pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
